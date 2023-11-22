457c42ea3d94140a7362347e907876a526d0cd2f
Vaibhav Anchal <vaibhav.anchal16@gmail.com>
2023-11-22 13:13:30 +0530

    Fixed form submit issue on enter

--- components/ConnectForm.js
+++ components/ConnectForm.js
@@ -9,9 +9,10 @@ import { imageUpload } from "./Helpers/ImageUpload";
 
 const ConnectForm = (props) => {
   const router = useRouter();
+  const formRef = React.useRef(null);
   const [step, setStep] = useState(1);
   const [file, setFile] = useState(null);
-  const manageForm = (e, type, step) => {
+  const manageForm = async (e, type, step) => {
     if (step < 2 && type === "NEXT") {
       setTimeout(function () {
         setStep((step) => step + 1);
@@ -21,6 +22,27 @@ const ConnectForm = (props) => {
       setTimeout(function () {
         setStep((step) => step - 1);
       }, 500);
+    }
+    if (step === 2 && !type) {
+      const { name, email, phone, address, message } = formRef.current;
+      try {
+        const { data } = await client.mutate({
+          mutation: SENDFORM,
+          variables: {
+            name: name.value,
+            phoneNumber: phone.value,
+            address: address.value,
+            message: message.value,
+            attachment: file?.url,
+            email: email.value,
+          },
+        });
+        if (data) {
+          window.location.href = `${window.location.origin}/thankyou`;
+        }
+      } catch (error) {
+        console.log(error);
+      }
     }
   };
 
@@ -38,34 +60,7 @@ const ConnectForm = (props) => {
   return (
     <div className="form-wrapper" {...props}>
       <div className="form-inner">
-        <form
-          className="contact-form"
-          onSubmit={async (e) => {
-            const { name, email, phone, address, message } = e.target;
-            e.preventDefault();
-            if (step === 2) {
-              try {
-                const { data, loading, error } = await client.mutate({
-                  mutation: SENDFORM,
-                  variables: {
-                    name: name.value,
-                    phoneNumber: phone.value,
-                    address: address.value,
-                    message: message.value,
-                    attachment: file?.url,
-                    email: email.value,
-                  },
-                });
-                if (data) {
-                  // router.push(`/thankyou/?${data?.createFormEntry?._id}`);
-                  window.location.href = `${window.location.origin}/thankyou`;
-                }
-              } catch (error) {
-                console.log(error);
-              }
-            }
-          }}
-        >
+        <form className="contact-form" ref={formRef} id="submitform">
           <div className={step === 1 ? `show` : `hide`}>
             <div className="fields-wrapper">
               <label> Name </label>
@@ -113,9 +108,9 @@ const ConnectForm = (props) => {
             )}{" "}
             <button
               disabled={file?.loading ?? false}
-              type={step === 2 ? `submit` : `button`}
+              type={`button`}
               className={step === 2 ? `submit` : ``}
-              onClick={(e) => manageForm(e, "NEXT", step)}
+              onClick={(e) => manageForm(e, step !== 2 ? "NEXT" : null, step)}
             >
               <span>{step === 1 ? `Next` : "Submit"}</span>
             </button>
